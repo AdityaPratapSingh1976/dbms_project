3c631214d01ad6bd11286cacd3240bd1d7bdb06f
AdityaPratapSingh1976 <128790690+AdityaPratapSingh1976@users.noreply.github.com>
2024-04-17 14:01:09 +0530

    Update aggregatefxns.sql

--- sql codes/dml code/aggregatefxns.sql
+++ sql codes/dml code/aggregatefxns.sql
@@ -40,23 +40,3 @@ EXTRACT(month FROM date_of_birth) AS birth_month, EXTRACT(year FROM date_of_birt
 FROM Professors;
 
 
---INNER JOIN-
-SELECT s.department_id, s.student_id, s.first_name, c.course_name FROM students s
-JOIN courses c ON c.department_id = s.department_id;
-
---LEFT OUTER JOIN-
-SELECT Students.department_id, Students.student_id, Courses.course_name, Courses.credits
-FROM Students
-LEFT OUTER JOIN Courses ON Students.department_id = Courses.department_id;
-  -- RIGHT OUTER JOIN-
-SELECT *
-FROM Professors
-RIGHT OUTER JOIN Departments ON Professors.department_id = Departments.department_id;
--- FULL OUTER JOIN- 
-SELECT *
-FROM Professors
-FULL OUTER JOIN Departments ON Professors.department_id = Departments.department_id;
--- NATURAL JOIN-
-SELECT department_id,student_id,first_name,course_name
-FROM students
-NATURAL JOIN courses;
